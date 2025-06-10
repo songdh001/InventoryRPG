1e30c47a0ab7b29e996e924f23c4ac7101b73997
songdh001 <155062686+songdh001@users.noreply.github.com>
2025-06-10 19:50:10 +0900

    에셋 추가, Player 캐릭터 추가

---
+++ Assets/Scripts/Entity/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : BaseController
+{
+    private Camera camera;
+
+    protected override void Start()
+    {
+        base.Start();
+        camera = Camera.main;
+    }
+
+
+    protected override void HandleAction()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        movementDirection = new Vector2(horizontal, vertical).normalized;
+
+        Vector2 mousePosition = Input.mousePosition;
+        Vector2 worldPos = camera.ScreenToWorldPoint(mousePosition);
+        lookDirection = (worldPos - (Vector2)transform.position);
+
+        if(lookDirection.magnitude < .9f)
+        {
+            lookDirection = Vector2.zero;
+        }
+        else
+        {
+            lookDirection = lookDirection.normalized;
+        }
+    }
+}
